e2a0a01992d8ef380e72cefea4920e46f781d4fb
Praveen880890 <s.praveenchowdarysureddy@gmail.com>
2023-01-06 20:57:21 +0530

    add level 9

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
 const express = require("express");
+var csrf = require("tiny-csrf");
+var cookieParser = require("cookie-parser");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path=require("path");
+const path = require("path");
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,'public')));
-app.get("/", async (request, response) =>{
-  const allTodos = await Todo.getTodos();
-  if (request.accepts("html")){
-    response.render('index',{
-      allTodos
-    });
+app.set("view engine", "ejs");
+app.get("/", async (request, response) => {
+  try {
+    const overduetodos = await Todo.overdue();
+    const duetodaytodos = await Todo.dueToday();
+    const duelatertodos = await Todo.dueLater();
+    const completedtodos = await Todo.completedTodos();
+
+    if (request.accepts("html")) {
+      response.render("index", {
+        title: "To-Do Manager",
+        overduetodos,
+        duetodaytodos,
+        duelatertodos,
+        completedtodos,
+        csrfToken: request.csrfToken(),
+      });
+    } else {
+      response.json({
+        overduetodos,
+        duetodaytodos,
+        duelatertodos,
+        completedtodos,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
   }
-  else{
-    response.json({
-      allTodos
-    });
-  }
-  response.render('index');
+});
+
+app.get("/", function (request, response) {
+  response.send("Hello World");
 });
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
- try{
-  const todo =await Todo.findAll();
-  response.send(todo);
- }
- catch (error){
-  console.log(error);
-  return response.status(422).json(error);
- }
+  try {
+    const todos = await Todo.findAll({
+      order: [["id", "ASC"]],
+    });
+    return response.json(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send(error);
+  }
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -46,18 +71,22 @@ app.get("/todos/:id", async function (request, response) {
 
 app.post("/todos", async function (request, response) {
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async function (request, response) {
+app.put("/todos/:id", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
-    const updatedTodo = await todo.markAsCompleted();
+    const todo = await Todo.findByPk(request.params.id);
+    const updatedTodo = await todo.setCompletionStatus(request.body.completed);
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
@@ -67,18 +96,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
-  try{
-  const deletedTodo = await Todo.destroy({
-    where:{
-      id: request.params.id
-    }
-  });
-  response.send(true);
-}
-catch (error){
-  console.log(error);
-  response.status(200).json(error);
-}
+
+  try {
+    const result = await Todo.remove(request.params.id);
+    return response.json({ success: result == 1 });
+  } catch (error) {
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
