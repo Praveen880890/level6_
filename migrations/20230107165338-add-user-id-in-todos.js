f9eb53207054c9c1e04597d2a5f60561cd3a9de8
Praveen880890 <s.praveenchowdarysureddy@gmail.com>
2023-01-08 20:04:05 +0530

    migration_problem_fixed

--- migrations/20230107165338-add-user-id-in-todos.js
+++ migrations/20230107165338-add-user-id-in-todos.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Todos','userId',{
-      type: Sequelize.DataTypes.INTEGER
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Todos", "userId", {
+      type: Sequelize.DataTypes.INTEGER,
     });
-    await queryInterface.addConstraint('Todos',{
-      fields: ['userId'],
-      type:'foreign key',
-      references:{
-        table:'Users',
-        feild:'id'
-      }
-    })
+    await queryInterface.addConstraint("Todos", {
+      fields: ["userId"],
+      type: "foreign key",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+    });
     /**
      * Add altering commands here.
      *
@@ -21,14 +22,13 @@ module.exports = {
      */
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Todos','userId')
-    
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Todos", "userId");
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
