ca09930d47f05be80d9f0733976b8ca64470cc34
Praveen880890 <s.praveenchowdarysureddy@gmail.com>
2023-01-08 20:05:43 +0530

    problem_fixed

--- migrations/20230107165338-add-user-id-in-todos.js
+++ migrations/20230107165338-add-user-id-in-todos.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Todos", "userId", {
+    await queryInterface.addColumn("Todos", {
       type: Sequelize.DataTypes.INTEGER,
     });
     await queryInterface.addConstraint("Todos", {
